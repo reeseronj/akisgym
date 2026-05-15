ccc38750de2bdeb6fb01776a0993d78c4024bcf0
reeseronj <reyesronjoshua.com>
2026-05-16 00:27:00 +0800

    asdasdasd

--- auth/login.php
+++ auth/login.php
@@ -21,10 +21,10 @@
 
             <form action="process_login.php" method="POST" class="login-form">
                 <div class="form-group">
-                    <label for="username">Email</label>
+                    <label for="Email">Email</label>
                     <input 
                         type="text"
-                        id="username"
+                        id="Email"
                         name="email"
                         placeholder="Enter your email"
                         required
